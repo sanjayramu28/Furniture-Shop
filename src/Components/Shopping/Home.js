6554f023d9f1097a7f2f596ba3e93f62c85f950c
Sanjay <sanjayrajamani2003@gmail.com>
2024-04-07 14:50:09 +0530

    Second

--- src/Components/Shopping/Home.js
+++ src/Components/Shopping/Home.js
@@ -4,6 +4,10 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import home_chair from '../Assests/Home_chair.png';
 import sofa from '../Assests/Sofa.png';
+import bed from '../Assests/bedroomfurnitures.jpg';
+import living from'../Assests/living funrniture.jpg';
+import dining from '../Assests/Dinner Furnitures.jpg';
+import office from '../Assests/office chairs.jpg';
 // import { Routes, Route } from "react-router-dom";
 const Home = () => {
 
@@ -21,6 +25,29 @@ const Home = () => {
             p2: 'Creative Sofas'
         }
 
+    ]
+
+    const Home_links=[
+        {
+            key:1,
+            img:bed,
+            title:'Bedroom'
+        },
+        {
+            key:2,
+            img:living,
+            title:'Living '
+        },
+        {
+            key:3,
+            img:dining,
+            title:'Dining'
+        },
+        {
+            key:4,
+            img:office,
+            title:'Office Chairs'
+        }
     ]
     // const observer=new IntersectionObserver((entries)=>{
     //     entries.forEach((entry)=>{
@@ -50,7 +77,6 @@ const Home = () => {
                                     <div className="col-md-6 products">
                                         <div className="">
                                             <p>{pro.p1}</p>
-                                            <p>jhh</p>
                                             <p>{pro.p2}</p>
                                             <br />
                                             <label>Torem ipsumsdfs shdgjhd hbhcfg</label>
@@ -64,22 +90,25 @@ const Home = () => {
                         )
                     })}
             </Carousel>
-            {/* <br></br>
-            <div className="pre-pro">
-                <div className="row pre-left">
-                    <div className="col-md-6 products">
-                        <div className="pro-info">
-                            <p>Best Seller</p>
-                            <p>Creative sofa</p>
-                            <br />
-                            <label>Torem ipsumsdfs shdgjhd hbhcfg</label>
-                        </div>
-                    </div>
-                    <div className="col-md-6 products">
-                        <img src={home_chair} className="home-img animate" />
-                    </div>
-                </div>
-            </div> */}
+            <div className="home-products mt-5">
+                {
+                    Home_links.map((link)=>{
+                        return(
+                            <div className="link">
+                            <img src={link.img} className="Home-link-img"/>
+                            {/* <br/> */}
+                            <p>{link.title}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <p>jhkh</p>
+            <p>jhkh</p>
+            <p>jhkh</p>
+            <p>jhkh</p>
+            <p>jhkh</p>
+            <p>jhkh</p>
         </div>
     )
 }
